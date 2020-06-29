7e801bd74831b0f5e26ebc2e4d539677825267be
Sebastien <work.sebastien.david@gmail.com>
2020-06-29 18:07:25 +0200

    Initial commit

---
+++ application.js
@@ -0,0 +1,26 @@
+function yetiTransition(){
+  var tl = new TimelineMax();
+  tl.from('#layer9', 0.4, {scaleX: 0, transformOrigin: "left", ease: Power2.easeOut});
+  tl.from('#layer10', 0.4, {scaleX: 0, transformOrigin: "left", ease: Power2.easeOut});
+  tl.from('#layer11', 0.4, {scaleX: 0, transformOrigin: "left", ease: Power2.easeOut});
+  tl.from('#layer13', 0.5, {delay: 1, scaleY: 0, transformOrigin: "bottom", ease: Power2.easeOut});
+  tl.from('#layer14', 1, {scaleX: 0, transformOrigin: "bottom", ease: "bounce.out"});
+  yetiBlinking()
+}
+
+
+
+function yetiBlinking(){
+  setInterval(function(){
+    var tl = new TimelineMax();
+    tl.to('#layer13', 0.5, {delay: 2, transformOrigin: "bottom", ease: Power2.easeOut})
+    tl.to('#layer13', 0.5, {scaleY: 0, transformOrigin: "bottom", ease: Power2.easeOut})
+    tl.to('#layer13', 0.5, {scaleY: 1, transformOrigin: "bottom", ease: Power2.easeOut})
+  }, 3500);
+}
+
+yetiTransition()
+
+setTimeout(function(){
+  document.querySelector(".home-title").style.display = "block"
+},8000)
